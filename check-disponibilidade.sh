aa01cfa29081733e27a28a0bd1075eb6c94a4681
mpalenzuela1012 <marcelo.palenzuela@innovsysti.com>
2026-09-25 23:07:45 +0000

    Deploy com CodePipeline

---
+++ check-disponibilidade.sh
@@ -0,0 +1,3 @@
+url="http://bia-alb-857058077.us-east-1.elb.amazonaws.com"
+docker build -t check_disponibilidade -f Dockerfile_checkdisponibilidade .
+docker run --rm -ti -e URL=$url check_disponibilidade
