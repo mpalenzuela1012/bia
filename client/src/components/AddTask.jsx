2613201f84c12544552a8fe2b6780ce71cfc5703
mpalenzuela1012 <marcelo.palenzuela@innovsysti.com>
2026-09-26 16:45:04 +0000

    Teste com ambiente Dev/Prod

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Add New Task (Task=Atividade, ok jovem?)
+        Add New Task em Prod (Task=Atividade, ok jovem?)
       </button>
       
       <Modal
